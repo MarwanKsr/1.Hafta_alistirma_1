541429c995f3fac0b9a77c4b88996d409e6717b5
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-21 00:21:59 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace alistirma_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi, soyad;
+            Console.Write("Adiniz: ");
+            adi = Console.ReadLine();
+            Console.Write("Soyadiniz: ");
+            soyad = Console.ReadLine();
+            Console.WriteLine("Merhaba " + adi + " " + soyad);
+        }
+    }
+}
